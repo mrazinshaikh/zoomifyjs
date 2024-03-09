b2104a582067dca9893ba6623a2e5a3505d38335
Oriol Parcerisa <oriol.p@floraqueen.com>
2024-03-09 15:01:42 +0100

    Merge with master

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,26 @@
 import { defineConfig } from 'vite';
 import terser from '@rollup/plugin-terser';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
   build: {
     lib: {
       entry: './src/zoomifyJs.js',
       name: 'ZoomifyJs',
     },
-    minify: 'terser',
+    minify: isProd ? 'terser' : false,
     rollupOptions: {
       output: {
         chunkFileNames: 'zoomifyJs',
         format: ['es', 'umd'],
-        plugins: [terser()],
+        plugins: isProd ? [terser()] : [],
       },
     },
     terserOptions: {
       compress: {
-        drop_console: true,
-        drop_debugger: true
+        drop_console: isProd,
+        drop_debugger: isProd
       }
     }
   },
